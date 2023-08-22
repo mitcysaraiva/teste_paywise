5bc6ad13f4089e35049e75c0b31b7bad52bfe932
mitcy <saraivamitcy@gmail.com>
2023-08-22 10:36:55 -0300

    atualizacoes

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+ 
+
+def index(request):
+    return render(request, 'app/index.html')
+
+def index(request):
+    return render(request, 'app/sobre.html')
+
+def index(request):
+    return render(request, 'app/equipe.html')
+
